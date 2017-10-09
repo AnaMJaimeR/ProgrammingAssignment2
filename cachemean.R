32a16f129d85c10b0e247e7f6fb36046a9e28aac
Ana.JR <anamariajaimer@gmail.com>
2017-10-08 22:28:24 -0500

    Example with mean

---
+++ cachemean.R
@@ -0,0 +1,29 @@
+##example for Assignment No 2. Course: R Programming
+##For further explanaiton go to
+## https://github.com/lgreski/datasciencectacontent/blob/master/markdown/rprog-breakingDownMakeVector.md
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+m <- x$getmean()
+if(!is.null(m)) {
+  message("getting cached data")
+  return(m)
+}
+data <- x$get()
+m <- mean(data, ...)
+x$setmean(m)
+m
+}
